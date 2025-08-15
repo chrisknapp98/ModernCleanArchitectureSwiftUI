4630ae7a2d6664717cf7465cb28aed6c140ec77f
Christopher Knapp <chris.knapp@live.de>
2025-08-16 01:12:17 +0200

    add task 14

---
+++ Projects/Benchmark/Sources/Task-14/MoviesAPI/Geteways/MovieDetailsGateway.swift
@@ -0,0 +1,19 @@
+import Foundation
+import HTTPClient
+import MoviesDomain
+
+public final class MovieDetailsGateway: MovieDetailUseCaseProtocol {
+    private let client: DataFetching
+    private let decoder = JSONDecoder()
+    private let dateDormatter = DateFormatter()
+
+    public init(client: DataFetching) {
+        self.client = client
+        dateDormatter.dateFormat = "YYYY-MM-DD"
+        decoder.dateDecodingStrategy = .formatted(dateDormatter)
+        decoder.keyDecodingStrategy = .convertFromSnakeCase
+    }
+
+    public func fetchDetail(for movieID: MovieID) async throws -> MovieDetail {
+    }
+}

9156e945cc11dccc1d6647721a50524bf749d371
Christopher Knapp <chris.knapp@live.de>
2025-08-16 17:13:25 +0200

    remove irrelevant files

--- Projects/Benchmark/Sources/Task-10/Movies/MoviesList/MoviesCoordinator.swift
+++
@@ -1,8 +0,0 @@
-import MoviesDomain
-
-@MainActor
-public protocol MoviesCoordinator {
-    func showDetail(for movie: Movie)
-    func showDetail(for person: Person)
-    func showAddMovieToCustomList(for movie: Movie)
-}
--- Projects/Benchmark/Tests/Task-10/PersonDetailsViewViewModelTests.swift
+++ Projects/Benchmark/Tests/Task-10/PersonDetailsViewViewModelTests.swift
@@ -1,6 +1,7 @@
 import XCTest
 import Dependencies
 @testable import Task_10
+@testable import MoviesDomain
 @testable import UI
 
 @MainActor
